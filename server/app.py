92889d55e072d200d6d49911ad2843e7809674a5
Judgement_H <446995859@qq.com>
2021-02-03 17:19:16 +0800

    test

--- server/app.py
+++ server/app.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-   return '黄涵卓'
+   return '陈阳2'
 
 if __name__ == '__main__':
-   app.run()
+   app.run(host='0.0.0.0',
+      port= 3268,
+      debug=True)
