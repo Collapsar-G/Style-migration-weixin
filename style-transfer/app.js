e628de557a104128f151e04c282d916dd6afcf80
CcCeLian <1790560657@qq.com>
2021-02-04 11:20:16 +0800

    首页更新

--- style-transfer/app.js
+++ style-transfer/app.js
@@ -1,18 +1,6 @@
 //app.js
 App({
   onLaunch: function() {
-    wx.getSystemInfo({
-      success: e => {
-        this.globalData.StatusBar = e.statusBarHeight;
-        let capsule = wx.getMenuButtonBoundingClientRect();
-		if (capsule) {
-		 	this.globalData.Custom = capsule;
-			this.globalData.CustomBar = capsule.bottom + capsule.top - e.statusBarHeight;
-		} else {
-			this.globalData.CustomBar = e.statusBarHeight + 50;
-		}
-      }
-    })
   },
   globalData: {
     ColorList: [{
--- style-transfer/pages/index/index.js
+++ style-transfer/pages/index/index.js
@@ -4,7 +4,19 @@ const app = getApp()
 
 Page({
   data: {
-
+    elements: [{
+      title: '布局',
+      name: 'layout',
+      color: 'red',
+      icon: 'newsfill'
+    },
+    {
+      title: '布局',
+      name: 'layout',
+      color: 'pink',
+      icon: 'newsfill'
+    }
+  ],
   },
 
   onLoad() {
