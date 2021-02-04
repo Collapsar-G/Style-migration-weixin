f1f5a35fdcfc416fe304a3e7742334f22e0cd4c7
CcCeLian <1790560657@qq.com>
2021-02-04 13:13:43 +0800

    选择图片和风格选择功能样式实现

--- style-transfer/app.js
+++ style-transfer/app.js
@@ -1,6 +1,14 @@
 //app.js
 App({
   onLaunch: function() {
+    wx.getSystemInfo({
+      success: e => {
+        this.globalData.StatusBar = e.statusBarHeight;
+        let custom = wx.getMenuButtonBoundingClientRect();
+        this.globalData.Custom = custom;  
+        this.globalData.CustomBar = custom.bottom + custom.top - e.statusBarHeight;
+      }
+    })
   },
   globalData: {
     ColorList: [{
