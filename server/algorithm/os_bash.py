de3be2c7730ff3a1b3b4c1bba8ed3d6b98fc6c6c
叶曦禾 <55824353+yexihe-jpg@users.noreply.github.com>
2021-02-05 16:17:18 +0800

    更新了test文件

--- server/algorithm/os_bash.py
+++ server/algorithm/os_bash.py
@@ -1,4 +1,15 @@
 import os
-content = './input/content/chicago.jpg'
+from test import using
+
+content = './input/content/flowers.jpg'
 style = './input/style/harvard_0.jpg'
-os.system("python test.py  --content  %s  --style %s" % (content, style))
+alpha = 1.0
+
+
+# os.system("python test.py  --content  %s  --style %s --alpha %f"  % (content, style,alpha))
+# def Style(content_path,style_path,alpha=1.0):
+def Style_():
+    return using(content, style, alpha)
+
+if __name__ == '__main__':
+    print(Style_())
