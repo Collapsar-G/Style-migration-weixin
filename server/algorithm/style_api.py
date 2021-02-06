97ba115cdafb419cc38095335d70ccaf05572685
叶曦禾 <55824353+yexihe-jpg@users.noreply.github.com>
2021-02-06 15:15:33 +0800

    更新了style接口

---
+++ server/algorithm/style_api.py
@@ -0,0 +1,122 @@
+import os
+from PIL import Image
+import time
+import numpy as np
+import cv2
+from algorithm.test import using_model
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+import cv2
+import requests
+import simplejson
+import time
+import base64
+from flask import Blueprint, render_template
+
+style = Blueprint('style', __name__)
+
+
+# os.system("python test.py  --content  %s  --style %s --alpha %f"  % (content, style,alpha))
+# def Style(content_path,style_path,alpha=1.0):
+def Style_no(content, style, alpha):
+    return using_model(content, style, alpha)
+
+
+def Style_is(content, style, alpha, preserve_color):
+    return using_model(content, style, alpha, preserve_color)
+
+
+def getByte(path):
+    with open(path, 'rb') as f:
+        img_byte = base64.b64encode(f.read())
+    img_str = img_byte.decode('ascii')
+    return img_str
+
+
+def tojson(index, result):
+    return {
+        'alpha' + index: str(result)
+    }
+
+
+@style.route('/style_qlssh_no/', methods=['post'])  # 青山绿水图不保留原色 接口
+def style_glssh_no():
+    if not request.data:  # 检测是否有数据
+        return 'fail'
+    data = request.data.decode('utf-8')
+    # 获取到POST过来的数据，
+    data_json = simplejson.loads(data)
+    # 把区获取到的数据转为JSON格式。
+    img_str = data_json['comment_img']
+    alpha = data_json['alpha']
+    img_decode_ = img_str.encode('ascii')  # ascii编码
+    img_decode = base64.b64decode(img_decode_)  # base64解码
+    img_np = np.frombuffer(img_decode, np.uint8)  # 从byte数据读取为np.array形式
+    img = cv2.imdecode(img_np, cv2.COLOR_RGB2BGR)  # 转为OpenCV形式
+    a = time.time()
+    # 显示图像
+    path = 'algorithm/using/' + str(a) + '.jpg'
+    cv2.imwrite(path, img)
+    print(path)
+    data = []
+
+    result = Style_no(path, 'algorithm/input/style_in/qlssh.jpg', alpha=alpha)
+    print(result)
+    img_str = getByte(result)
+    data.append(tojson(str(alpha), str(img_str)))
+
+    return simplejson.dumps(data, ensure_ascii=False)
+
+
+@style.route('/style_qlssh_is/', methods=['post'])  # 青山绿水图保留原色 接口
+def style_glssh_is():
+    if not request.data:  # 检测是否有数据
+        return 'fail'
+    data = request.data.decode('utf-8')
+    # 获取到POST过来的数据，
+    data_json = simplejson.loads(data)
+    # 把区获取到的数据转为JSON格式。
+    img_str = data_json['comment_img']
+    alpha = img_str['alpha']
+    img_decode_ = img_str.encode('ascii')  # ascii编码
+    img_decode = base64.b64decode(img_decode_)  # base64解码
+    img_np = np.frombuffer(img_decode, np.uint8)  # 从byte数据读取为np.array形式
+    img = cv2.imdecode(img_np, cv2.COLOR_RGB2BGR)  # 转为OpenCV形式
+    a = time.time()
+    # 显示图像
+    path = 'algorithm/using/' + str(a) + '.jpg'
+    cv2.imwrite(path, img)
+    print(path)
+    data = []
+   
+    result = Style_is(path, 'algorithm/input/style_in/qlssh.jpg', alpha, True)
+    print(result)
+    img_str = getByte(result)
+    data.append(tojson(str(alpha), str(img_str)))
+
+    return simplejson.dumps(data, ensure_ascii=False)
+
+
+# 
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0',
+#             port=3268,
+#             debug=True)
+
+if __name__ == '__main__':
+    # content = './input/content/brad_pitt.jpg'
+    content = 'C:/Users/20180/Desktop/golden_gate.jpg'
+    times = time.time()
+
+    style = 'C:/Users/20180/desktop/qjss.png'
+    alpha = 1.0
+
+    # img = cv2.imread(content)
+    # img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    # img = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2RGB)
+    # cv2.imwrite("./output/" + str(times) + '.jpg',img)
+    # result = Style_("./output/" + str(times) + '.jpg', style, alpha)
+    result = Style_(content, style, alpha)
+    img_result = cv2.imread(str(result))
+    cv2.imshow('image', img_result)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
---
+++ server/algorithm/test.py
@@ -0,0 +1,142 @@
+import argparse
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+from PIL import Image
+from torchvision import transforms
+from torchvision.utils import save_image
+
+from algorithm import  net
+from algorithm.function import adaptive_instance_normalization, coral
+# import net
+# from function import adaptive_instance_normalization, coral
+
+
+def test_transform(size, crop):
+    transform_list = []
+    if size != 0:
+        transform_list.append(transforms.Resize(size))
+    if crop:
+        transform_list.append(transforms.CenterCrop(size))
+    transform_list.append(transforms.ToTensor())
+    transform = transforms.Compose(transform_list)
+    return transform
+
+
+def style_transfer(vgg, decoder, content, style, alpha=1.0,
+                   interpolation_weights=None):
+    assert (0.0 <= alpha <= 1.0)
+    content_f = vgg(content)
+    style_f = vgg(style)
+    if interpolation_weights:
+        _, C, H, W = content_f.size()
+        feat = torch.FloatTensor(1, C, H, W).zero_().to(device)
+        base_feat = adaptive_instance_normalization(content_f, style_f)
+        for i, w in enumerate(interpolation_weights):
+            feat = feat + w * base_feat[i:i + 1]
+        content_f = content_f[0:1]
+    else:
+        feat = adaptive_instance_normalization(content_f, style_f)
+    feat = feat * alpha + content_f * (1 - alpha)
+    return decoder(feat)
+
+
+def using_model(content, style, alpha, preserve_color=False):
+    vgg_path = 'algorithm/models/vgg_normalised.pth'
+    decoder_path = 'algorithm/models/decoder.pth'
+    # vgg_path = './models/vgg_normalised.pth'
+    # decoder_path = './models/decoder.pth'
+    content_dir = ''
+    style_dir = ''
+    content_size = 512  # New (minimum) size for the content image, keeping the original size if set to 0
+    style_size = 512  # New (minimum) size for the style image, keeping the original size if set to 0
+    save_ext = '.jpg'  # The extension name of the output image
+    output = 'output'  # Directory to save the output image(s)
+    style_interpolation_weights = ''  # The weight for blending the style of multiple style images
+    do_interpolation = True
+    crop = True
+
+    do_interpolation = False
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    output_dir = Path(output)
+    output_dir.mkdir(exist_ok=True, parents=True)
+
+    # Either --content or --contentDir should be given.
+    assert (content or content_dir)
+    if content:
+        content_paths = [Path(content)]
+    else:
+        content_dir = Path(content_dir)
+        content_paths = [f for f in content_dir.glob('*')]
+
+    # Either --style or --styleDir should be given.
+    assert (style or style_dir)
+    if style:
+        style_paths = style.split(',')
+        if len(style_paths) == 1:
+            style_paths = [Path(style)]
+        else:
+            do_interpolation = True
+            assert (style_interpolation_weights != ''), \
+                'Please specify interpolation weights'
+            weights = [int(i) for i in style_interpolation_weights.split(',')]
+            interpolation_weights = [w / sum(weights) for w in weights]
+    else:
+        style_dir = Path(style_dir)
+        style_paths = [f for f in style_dir.glob('*')]
+
+    decoder = net.decoder
+    vgg = net.vgg
+
+    decoder.eval()
+    vgg.eval()
+
+    decoder.load_state_dict(torch.load(decoder_path))
+    vgg.load_state_dict(torch.load(Path(vgg_path)))
+    vgg = nn.Sequential(*list(vgg.children())[:31])
+
+    vgg.to(device)
+    decoder.to(device)
+
+    content_tf = test_transform(content_size, crop)
+    style_tf = test_transform(style_size, crop)
+
+    for content_path in content_paths:
+        if do_interpolation:  # one content image, N style image
+            style = torch.stack([style_tf(Image.open(str(p))) for p in style_paths])
+            content = content_tf(Image.open(str(content_path))) \
+                .unsqueeze(0).expand_as(style)
+            style = style.to(device)
+            content = content.to(device)
+            with torch.no_grad():
+                output = style_transfer(vgg, decoder, content, style,
+                                        alpha, interpolation_weights)
+            output = output.cpu()
+            output_name = output_dir / '{:s}_stylized_{:s}_alpha_{:s}{:s}'.format(
+                content_path.stem, style_path.stem, str(alpha), save_ext)
+            save_image(output, str(output_name))
+
+        else:  # process one content and one style
+            for style_path in style_paths:
+                content = content_tf(Image.open(str(content_path)))
+                style = style_tf(Image.open(str(style_path)))
+                if preserve_color:
+                    style = coral(style, content)
+                style = style.to(device).unsqueeze(0)
+                content = content.to(device).unsqueeze(0)
+                with torch.no_grad():
+                    output = style_transfer(vgg, decoder, content, style,
+                                            alpha)
+                output = output.cpu()
+
+                output_name = output_dir / '{:s}_stylized_{:s}_alpha_{:s}{:s}'.format(
+                    content_path.stem, style_path.stem, str(alpha), save_ext)
+                save_image(output, str(output_name))
+        return output_name
+
+
+if __name__ == '__main__':
+    print(using_model('./input/content/test6.jpg', './input/style_in/test5.jpg', 1.0))
