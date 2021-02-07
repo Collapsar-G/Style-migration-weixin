7cabe445f909485920d0116f3784e4bc23e2f5fe
叶曦禾 <55824353+yexihe-jpg@users.noreply.github.com>
2021-02-07 14:07:11 +0800

    修改了transfer的返回参数

--- server/algorithm/img_post.py
+++ server/algorithm/img_post.py
@@ -20,17 +20,18 @@ if __name__ == '__main__':
     alpha = 1.0
     # print(img_str)
     # url = 'http://39.105.76.87:3268/style_qlssh_no/'
-    # url = 'http://xcx.collapsar.online/style_qlssh_is/'
-    url = 'http://localhost:3268/style_qjssh_is/'
+    url = 'https://xcx.collapsar.online/ style_qlssh_is/'
+    # url = 'http://localhost:3268/style_qjssh_is/'
     data = {'comment_img': img_str,'alpha': alpha}
     json_mod = simplejson.dumps(data)
     res = requests.post(url=url, data=json_mod)
-    # print(res.text)
+    print(res.text)
     data_json = simplejson.loads(res.text)
     print(data_json)
     # 把区获取到的数据转为JSON格式。
 
     img_str = data_json[0]['alpha' + str(alpha)]
+    print(img_str)
     img_decode_ = img_str.encode('ascii')  # ascii编码
     img_decode = base64.b64decode(img_decode_)  # base64解码
     img_np = np.frombuffer(img_decode, np.uint8)  # 从byte数据读取为np.array形式
--- server/api/transfer.py
+++ server/api/transfer.py
@@ -24,7 +24,7 @@ STYLE_QLSSH_PATH = 'static/image/style/qlssh.jpg'
 STYLE_QJSSH_PATH = 'static/image/style/qjssh.jpg'
 # HOST = '127.0.0.1:3268'
 # HOST = '39.105.76.87:3268'
-HOST = 'https://xcx.collapsar.online:3268'
+HOST = 'https://xcx.collapsar.online'
 
 @transfer.route('/style_qlssh_no', methods=['POST'])
 def style_qlssh_no():
