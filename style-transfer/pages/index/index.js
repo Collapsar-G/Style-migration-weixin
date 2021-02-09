84006370978302f047227e86f8cf20e578e187b7
CcCeLian <1790560657@qq.com>
2021-02-09 18:25:39 +0800

    添加用户信息获取，新建功能、优化完成

--- style-transfer/pages/index/index.js
+++ style-transfer/pages/index/index.js
@@ -20,10 +20,60 @@ Page({
     src:"../../icon/btn.png",
     animationData:{},
     hidden:false,
-    TheOpacity: 1
+    TheOpacity: 1,
+    canIUse:wx.canIUse('button.open-type.getUserInfo')
   },
   onLoad() {
-
+    // 查看是否授权
+    let that = this
+    wx.getSetting({
+      success (res){
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          that.Login()
+          wx.getUserInfo({
+            success: function(res) {
+              console.log(res.userInfo)
+            }
+          })
+          that.setData({
+            canIUse:false
+          })
+        }
+      }
+    })
+  },
+  Login:function(){
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+        var appid = 'wx7330267cab93fa97'; //填写微信小程序appid
+        var secret = 'd2d27f80db202377fbb8ca9d49eab458'; //填写微信小程序secret
+        //调用request请求api转换登录凭证
+        wx.request({
+          url: 'https://xcx.collapsar.online/user/login',
+          data:{
+            'code':res.code
+          },
+          method:'POST',
+          header: {
+            'content-type': 'application/json'
+          },
+          success: function (res) {
+            console.log(res) //获取openid
+            app.globalData.openid = res.data.id;
+          }
+        })
+      }
+    })
+  },
+  bindGetUserInfo (e) {
+    if(e.detail.userInfo != null){
+      this.Login();
+      this.setData({
+        canIUse:false
+      })
+    }
   },
   bindChange:function(e){
     let that = this
@@ -33,13 +83,10 @@ Page({
     })
 
     this.animation = animation
- 
     animation.opacity(0.5).step()
- 
     this.setData({
       animationData: animation.export()
     })
- 
     setTimeout(function () {
       wx.navigateTo({
         url: '../create/create'
